85b62d3ad7a09484f1453cb2cb40183c6b3bcea5
OrNixz <ramadhanafif172@gmail.com>
2024-01-21 14:26:16 +0700

    Implemented ejs-mate for layouting

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const ejsMate = require("ejs-mate");
 const express = require("express");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
@@ -17,6 +18,7 @@ mongoose
     console.log(err);
   });
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
