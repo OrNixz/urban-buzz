febdad77fda496bf55aa2d8bc51457bf785e5947
OrNixz <ramadhanafif172@gmail.com>
2024-01-24 11:29:48 +0700

    Create async wrapper handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ejsMate = require("ejs-mate");
 const express = require("express");
 const methodOverride = require("method-override");
+const wrapAsync = require("./utils/wrapAsync");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
@@ -30,44 +31,62 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/places", async (req, res) => {
-  const places = await Place.find();
-  res.render("places/index", { places });
-});
+app.get(
+  "/places",
+  wrapAsync(async (req, res) => {
+    const places = await Place.find();
+    res.render("places/index", { places });
+  })
+);
 
 app.get("/places/create", (req, res) => {
   res.render("places/create");
 });
 
-app.post("/places", async (req, res, next) => {
-  try {
-    const place = new Place(req.body.place);
-    await place.save();
+app.post(
+  "/places",
+  wrapAsync(async (req, res, next) => {
+    try {
+      const place = new Place(req.body.place);
+      await place.save();
+      res.redirect("/places");
+    } catch (error) {
+      next(error);
+    }
+  })
+);
+
+app.get(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    const place = await Place.findById(req.params.id);
+    res.render("places/show", { place });
+  })
+);
+
+app.get(
+  "/places/:id/edit",
+  wrapAsync(async (req, res) => {
+    const place = await Place.findById(req.params.id);
+    res.render("places/edit", { place });
+  })
+);
+
+app.put(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
     res.redirect("/places");
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
-app.get("/places/:id", async (req, res) => {
-  const place = await Place.findById(req.params.id);
-  res.render("places/show", { place });
-});
-
-app.get("/places/:id/edit", async (req, res) => {
-  const place = await Place.findById(req.params.id);
-  res.render("places/edit", { place });
-});
-
-app.put("/places/:id", async (req, res) => {
-  await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
-  res.redirect("/places");
-});
-
-app.delete("/places/:id", async (req, res) => {
-  await Place.findByIdAndDelete(req.params.id);
-  res.redirect("/places");
-});
+app.delete(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    await Place.findByIdAndDelete(req.params.id);
+    res.redirect("/places");
+  })
+);
 
 // Error Handling
 app.use((err, req, res, next) => {
