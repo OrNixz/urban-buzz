91bcd69ba26333db162e53e1107ec37421dbcd70
OrNixz <ramadhanafif172@gmail.com>
2024-01-25 11:16:47 +0700

    Create a review model

--- models/place.js
+++ models/place.js
@@ -7,6 +7,10 @@ const placeSchema = new Schema({
   description: String,
   location: String,
   image: String,
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: "Review"
+  }]
 });
 
 module.exports = mongoose.model("Place", placeSchema);
