09b2efcfa833722cc521a071e06bf2dfa03966b6
OrNixz <ramadhanafif172@gmail.com>
2024-01-26 15:33:35 +0700

    Delete the place data along with the review data

--- models/place.js
+++ models/place.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
@@ -7,10 +8,22 @@ const placeSchema = new Schema({
   description: String,
   location: String,
   image: String,
-  reviews: [{
-    type: Schema.Types.ObjectId,
-    ref: "Review"
-  }]
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+placeSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Place", placeSchema);
