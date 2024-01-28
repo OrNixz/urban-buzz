8fd392cfe10302def6e996a3ff6ecbbbacaa58bf
OrNixz <ramadhanafif172@gmail.com>
2024-01-28 17:32:52 +0700

    Setup a session for flash messaging and authentication

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const path = require("path");
+const session = require("express-session");
 const app = express();
 
 // Connect to MongoDB
@@ -24,6 +25,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "thisisasecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.render("home");
