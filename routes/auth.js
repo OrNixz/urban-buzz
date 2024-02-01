6ee3153bf9708c06f29b6bc650fa02f469fb2fdc
OrNixz <ramadhanafif172@gmail.com>
2024-02-01 10:06:51 +0700

    Create a user login function and fix some issues

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("auth/register");
@@ -15,12 +16,30 @@ router.post(
       const user = new User({ email, username });
       await User.register(user, password);
       req.flash("success", "You are now registered! Please login.");
-      res.redirect("/places");
+      res.redirect("/login");
     } catch (error) {
       req.flash("error", error.message);
       res.redirect("/register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: {
+      type: "error",
+      message: "Invalid username or password.",
+    },
+  }), (req, res) => {
+    req.flash("success", "You are now logged in!");
+    res.redirect("/places");
+  }
 );
 
 module.exports = router;
